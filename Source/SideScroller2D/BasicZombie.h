fd7b3c97fc90764f0075a9ababb55b0ac59ab0fe
ARYANBro <nalinbro28@gmail.com>
2023-03-05 17:40:34 +0530

    Enemy

---
+++ Source/SideScroller2D/BasicZombie.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "BasicZombie.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIDESCROLLER2D_API ABasicZombie : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
